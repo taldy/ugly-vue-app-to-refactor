c0b2a3ed4b6d30d522e7ffb952295dc939ed26b3
taldy <taldykin.egor@gmail.com>
2019-08-28 12:36:58 +0300

    Ugly app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,75 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>GitHub explorer</h1>
+    <div v-if="selectedId">
+      <div v-for="item in results" v-if="item.id === selectedId">
+        <h2>Details</h2>
+        <div>Score: {{ item.score }}</div>
+        <div>Forks?: {{ item.fork ? 'true' : 'false' }}</div>
+        <div>Full Name: {{ item.full_name }}</div>
+        <button type="button" name="button" @click="cancel">Hide</button>
+      </div>
+    </div>
+    <div v-else>
+      <div>
+        <input type="text" v-model="searchText" @input="onChange" />
+        <select v-model="searchBy" @change="onChange">
+          <option value="repositories">repositories</option>
+          <option value="users">users</option>
+          <option value="issues">issues</option>
+        </select>
+      </div>
+      <ul>
+        <li v-for="item in results" @click="select(item.id)">
+          <div>{{ item.name }}</div>
+          <a :href="item.url">{{ item.url }}</a>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import axios from 'axios';
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      searchBy: 'repositories',
+      searchText: '',
+      results: [],
+      selectedId: null,
+    };
+  },
+  methods: {
+    search(byRepo, byUser, byIssues, text) {
+      if (byRepo) {
+        return axios.get(`https://api.github.com/search/repositories?q=${text}`);
+      }
+      if (byUser) {
+        return axios.get(`https://api.github.com/search/users?q=${text}`);
+      }
+      if (byIssues) {
+        return axios.get(`https://api.github.com/search/issues?q=${text}`);
+      }
+    },
+    onChange() {
+      this.search(
+        this.searchBy === 'repositories',
+        this.searchBy === 'users',
+        this.searchBy === 'issues',
+        this.searchText,
+      ).then((res) => {
+        this.results = res.data.items;
+      });
+    },
+    select(id) {
+      this.selectedId = id;
+    },
+    cancel() {
+      this.selectedId = null;
+    },
   },
 };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
